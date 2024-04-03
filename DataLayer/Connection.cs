34e9ca4c53d00e4388cb356b92d9b3d00da4fadc
Hamit Can SAYILGAN <hamitcansayilgan@gmail.com>
2024-04-03 17:38:20 +0300

    - Feature:  Added SQL Connection

---
+++ DataLayer/Connection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUDNorthwindDB.DataLayer
+{
+	public class Connection
+	{
+		public static SqlConnection baglanti
+		{
+			get
+			{
+				SqlConnection sqlcon = new SqlConnection
+					("Server= .;Trusted_Connection=True;Database=NORTHWND;");
+				return sqlcon;
+			}
+		}
+
+	}
+}
